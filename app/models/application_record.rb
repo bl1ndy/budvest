34c22e2df4f6ff80c325068506d821db0a7d87b9
bl1ndy <ayoblindstone@gmail.com>
2023-03-10 15:17:59 +0300

    doc: add doc to ApplicationRecord.i18n_enum

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,11 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   class << self
+    # Returns formatted enum value according to current locale
+    #
+    # @param enum_name [String, Symbol] the name of the model enum attribute.
+    # @param enum_value [String, Symbol] possible value of the model enum attribute.
+    # @return [String] formated enum value.
     def i18n_enum(enum_name, enum_value)
       locale_key = enum_name.to_s.pluralize
       I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{locale_key}.#{enum_value}")
